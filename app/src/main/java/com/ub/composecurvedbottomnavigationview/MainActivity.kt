9c901658c4484cf69e79084ec62a203fcd192c7a
umair <saeedahmedasked@gmail.com>
2026-03-20 06:58:19 +0500

    dimension helper added

--- app/src/main/java/com/ub/composecurvedbottomnavigationview/MainActivity.kt
+++ app/src/main/java/com/ub/composecurvedbottomnavigationview/MainActivity.kt
@@ -8,6 +8,9 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.ub.composecurvedbottomnavigationview.screen.MainScreen
 import com.ub.composecurvedbottomnavigationview.screen.MainViewModel
 import com.ub.composecurvedbottomnavigationview.ui.theme.ComposeCurvedBottomNavigationViewTheme
@@ -19,6 +22,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
+            val insetsController = WindowCompat.getInsetsController(window, window.decorView)
+
+            insetsController.apply {
+                hide(WindowInsetsCompat.Type.statusBars())
+                hide(WindowInsetsCompat.Type.navigationBars())
+                systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+
+
             ComposeCurvedBottomNavigationViewTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
                     MainScreen(viewModel = viewModel)
